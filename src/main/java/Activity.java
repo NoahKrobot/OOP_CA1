e4727fdcfd7c15de2ca8b09c0fa21b9ea53f5fd5
NoahKrobot <D00259927@student.dkit.ie>
2023-10-27 09:37:23 +0100

    Getter and Setter methods created in Activity class.

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -17,6 +17,43 @@ public class Activity {
     }
 
 
+    public String getType_of_activity() {return type_of_activity;}
+
+    public void setType_of_activity(String type_of_activity) {this.type_of_activity = type_of_activity;}
+
+    public double getDuration() {return duration;}
+
+    public void setDuration(double duration) {this.duration = duration;}
+
+    public String getDate() {return date;}
+
+    public void setDate(String date) {this.date = date;}
+
+    public double getAvg_heart_rate() {return avg_heart_rate;}
+
+    public void setAvg_heart_rate(double avg_heart_rate) {this.avg_heart_rate = avg_heart_rate;}
+
+    public double getEnergy_spent() {return energy_spent;}
+
+    public void setEnergy_spent(double energy_spent) {this.energy_spent = energy_spent;}
+
+    public double getCalories_burnt() {return calories_burnt;}
+
+    public void setCalories_burnt(double calories_burnt) {this.calories_burnt = calories_burnt;}
+
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "type_of_activity='" + type_of_activity + '\'' +
+                ", duration=" + duration +
+                ", date='" + date + '\'' +
+                ", avg_heart_rate=" + avg_heart_rate +
+                ", energy_spent=" + energy_spent +
+                ", calories_burnt=" + calories_burnt +
+                '}';
+    }
+
     public Activity() {
         this.type_of_activity = "";
         this.duration = 0;
