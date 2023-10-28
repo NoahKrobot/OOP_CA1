13b29bde8a4f21fc1c5560d7044102a3dc8eff2d
shahzad shabeer <shahzadshabeer13@gmail.com>
2023-10-28 20:54:18 +0100

    test shahzad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world!");//test shahzad
 
         Activity act1 = new Activity("Swimming", 60,"13",1,13);
         act1.setIntensity();
